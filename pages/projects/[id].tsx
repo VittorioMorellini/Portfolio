ce20b7e6852e19bcd107e9f930e811f0e0e28c5c
sxi0147 <vmorellini70@gmail.com>
2023-03-05 12:42:07 +0100

    Sviluppo dettaglio personal e projects and secrets github

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -1,25 +1,59 @@
+import { StaticImageData } from "next/image";
 import { useRouter } from "next/router";
 import React from "react";
-//import { useNavigate } from "react-router-dom";
+import { Project } from "types/project";
+import ImageLoader from "utils/imageLoader";
+import { ProjectList } from "../../public/data/projects";
+import Image from 'next/image';
 
 interface ProjectItemProps {
-    name: string;
-    id: number;
-    image: any;
+    item: Project;
 }
 
-function ProjectItem({ image, name, id }: ProjectItemProps) {
+function ProjectItem({ item }: ProjectItemProps) {
   const router = useRouter();
+  const {id, skills, image, name, description } = item
 
   return (
     <div
-      className="text-center"
+      className="text-center h-full w-full"
     >
-      <h1>Detail of project personal {router.query.id}</h1>
-      <div style={{ backgroundImage: `url(${image})` }} className="bgImage" />
-      <h1> {name} </h1>
+      <h1>Personal project #{id} - <b>{name}</b></h1>
+      <div className="bg-transparent w-full h-96 pt-4 mx-auto">
+        <Image
+          loader={ImageLoader}
+          unoptimized
+          src={image}
+          alt={name}
+          width="400px"
+          height="300px"
+        />
+      </div>
+      <div className="pt-4">
+        <h1 className="text-2xl"> {name} </h1>
+      </div>
+      <div>
+        {skills}
+      </div>
+      <div className="pt-4">
+        {description}
+      </div>
     </div>
   );
 }
 
 export default ProjectItem;
+
+export async function getServerSideProps(context: any) {
+  const id = context.params?.id as string;
+  console.log('projects id', id)
+  //const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+  let item: Project | undefined = ProjectList.find(x => x.id.toString() === id)
+  //const res = data.results;
+  console.log('results data', item)
+  return {
+      props: {
+        item
+      }
+  };
+};
--- public/data/projects.ts
+++ public/data/projects.ts
@@ -9,23 +9,27 @@ export const ProjectList = [
     name: "Call center",
     image: Proj1,
     skills: "Asp.Net (C#), jQuery, Oracle",
+    description: 'My first web project by my Self. An interesting web application to manage call center and its articles selling on media'
   },
   {
     id: 2,
     name: "Mattresses orders",
     image: Proj2,
     skills: "Asp.Net (C#), jQuery, Oracle",
+    description: 'A good web project for a friend in Carpi that have a commercial activity for mattress and bedroom articles orders'
   },
   {
     id: 3,
     name: "Personal portfolio",
     image: Proj3,
     skills: "Next.js, Node.js, Tailwindcss",
+    description: 'Finally I\'ve got my portfolio to introduce myself on the world'
   },
   {
     id: 4,
     name: "Printed circuits",
     image: Proj4,
     skills: "Vb6, Pervasive",
+    description: 'A vb6 project that I follow for a company located in Modena to manage printed circuit offers'
   },
 ];
